b733aea045d241f2817e0178d4810a689964d286
Xu, Yuanhai (Solever, HPSW-R&D-SH) <yuanhai.xu@hpe.com>
2018-03-23 17:31:40 +0800

    Add github script to apply status on pull request

--- githubscripts/pull_requests/apply_pr_status.sh
+++ githubscripts/pull_requests/apply_pr_status.sh
@@ -3,30 +3,35 @@
 script_dir=$(dirname $(readlink -f $0))
 sh_name=$0
 
-sh_desc="Apply a force Github pull request status checks."
+sh_desc="Apply status on the head commit of pull request"
 sh_usage=$(cat <<-END
 
-Usage: "${sh_name}" [options] org/repo pr-id status-check1 [status-check2 ...]
+Usage: "${sh_name}" [options] org/repo pr-id
 
 positional parameter
   org/repo          organization name and repository name, separated by "/"
-  pr-id             pull request ID on which apply the force status check
-  status-check1..N  one or more status checks to set "success" status
+  pr-id             pull request ID on which the status will be applied
 
 options
-  -u USER           who request force PR status checks
-  -m MESSAGE        message to request force PR status checks
-  -r URL            status check target URL
+  -c CONTEXT        context of status to apply, defaults to 'default'
+
+  -p                set state to 'pending', this is the default value
+  -s                set state to 'success', higher priority than '-p'
+  -f                set state to 'failure', higher priority than '-s'
+  -e                set state to 'error', higher priority than '-f'
+
+  -d DESCRIPTION    short (one-line) description of the status
+  -u URL            target URL to associate with the status
 
   -b BASE_API_URL=https://github.houston.softwaregrp.net/api/v3
-                    base URL of Github API to apply PR status checks
+                    base URL of Github API to apply PR status
   -t GITHUB_TOKEN   token to be used to send Github API request, if omitted,
                     try environment variable 'GITHUB_USER_TOKEN' first and
                     then use default one
   -h                show this help text and exit
 
 Samples:
-  "${sh_name}" -u john -m "Fix a critical bug" myorg/test1 15 cd/release/cf
+  "${sh_name}" -sc ci/jenkins -d "CI #12" -u "http://url.com" myorg/test1 15
 END
 )
 
@@ -34,18 +39,32 @@ END
 ##################### GET COMMAND OPTIONS - START ####################
 
 OPTIND=1
-while getopts ":u:m:r:b:t:h" opt; do
+while getopts ":c:psfed:u:b:t:h" opt; do
     case $opt in
-        # Request user and message
+        # Status
+        c)
+            context="$OPTARG"
+            ;;
+        p)
+            state="pending"
+            ;;
+        s)
+            state="success"
+            ;;
+        f)
+            state="failure"
+            ;;
+        e)
+            state="error"
+            ;;
+        d)
+            description="$OPTARG"
+            ;;
         u)
-            req_user="$OPTARG"
+            url="$OPTARG"
             ;;
-        m)
-            req_msg="$OPTARG"
-            ;;
-        r)
-            status_check_url="$OPTARG"
-            ;;
+
+        # API
         b)
             base_api_url="$OPTARG"
             ;;
@@ -76,12 +95,12 @@ shift $((OPTIND-1))
 
 repo_full_name="$1"
 pr_id="$2"
-shift 2
 
 if [ -z "${repo_full_name}" ]; then echo "The organization and repository name is not specified." && echo "${sh_usage}"; exit 1; fi
 if [ -z "${pr_id}" ]; then echo "The pull request ID is not specified." && echo "${sh_usage}"; exit 2; fi
-if [ -z "${req_user}" ]; then echo "The request user is not specified." && echo "${sh_usage}"; exit 3; fi
-if [ -z "${req_msg}" ]; then echo "The request message is not specified." && echo "${sh_usage}"; exit 4; fi
+
+if [ -z "${context}" ]; then context="default"; fi
+if [ -z "${state}" ]; then state="pending"; fi
 
 if [ -z "${base_api_url}" ]; then base_api_url="https://github.houston.softwaregrp.net/api/v3"; fi
 if [ -z "${github_token}" ]; then github_token="${GITHUB_USER_TOKEN}"; fi
@@ -118,41 +137,34 @@ json=$(curl --silent \
 )
 if [ -z "$json" ]; then echo "Failed to send Github API request to get pull request."; exit 9; fi
 sha=$(echo "$json" | ./jq -r '.head.sha')
-echo "Commit sha: $sha"
 
 
-# apply status check
+# apply status
 # https://developer.github.com/v3/repos/statuses/#create-a-status
 req_method="POST"
 api_endpoint="/repos/${repo_full_name}/statuses/${sha}"
 full_api_url="${base_api_url}${api_endpoint}"
 
-while true; do
-    status_check="$1"
-    if [ -z "${status_check}" ]; then break; fi
-    shift
+data="{\"state\":\"${state}\""
+data="${data},\"context\":\"${context}\""
+if [ -n "${description}" ]; then
+    data="${data},\"description\":\"${description}\""
+fi
+if [ -n "${url}" ]; then
+    data="${data},\"target_url\":\"${url}\""
+fi
+data="$data}"
 
-    data='{"state":"success"'
-    data="${data},\"context\":\"${status_check}\""
-    data="${data},\"description\":\"Required by '${req_user}': ${req_msg}\""
-    if [ -n "${status_check_url}" ]; then
-        data="${data},\"target_url\":\"${status_check_url}\""
-    fi
-    data="$data}"
+http_code=$(curl --silent --output /dev/null --write-out %{http_code} \
+    --request "${req_method}" \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer ${github_token}" \
+    --data "${data}" \
+    "${full_api_url}"
+)
+if [ "${http_code}" != "201" ]; then
+    echo "Failed to send Github API request to apply status '${context}' on head commit '${sha}' of pull request '${pr_id}' in repository '${repo_full_name}'. HTTP CODE: ${http_code}"
+    exit 9
+fi
 
-    echo -n "${repo_full_name}, PR #${pr_id}, ${status_check} ... "
-    http_code=$(curl --silent --output /dev/null --write-out %{http_code} \
-        --request "${req_method}" \
-        --header 'Content-Type: application/json' \
-        --header "Authorization: Bearer ${github_token}" \
-        --data "${data}" \
-        "${full_api_url}"
-    )
-    if [ "${http_code}" = "201" ]; then
-        echo "Done"
-    else
-        echo "Failed"
-        echo "Failed to send Github API request to set status check '${status_check}' for pull request '${pr_id}' in repository '${repo_full_name}'. HTTP CODE: ${http_code}"
-        exit 9
-    fi
-done
+echo "Status '${context}' is applied on PR #${pr_id} (${repo_full_name}) successfully!"
