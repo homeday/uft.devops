38dcbbf4e7566d2f8cb9635752a8e26e19613938
Guo, Yi-Bin (Kim, HPSW-R&D-SH) <yguo@hpe.com>
2018-04-13 16:33:16 +0800

    update parameter's name

--- deployment/deploymentscripts/CSADeployment.ps1
+++ deployment/deploymentscripts/CSADeployment.ps1
@@ -3,23 +3,23 @@ using module ".\CSADeployment.psm1"
 [CmdletBinding(SupportsShouldProcess=$True)]
 param (
 	[Parameter(Mandatory=$true)]
-    [string]$VM_NAME = "",
+    [string]$CSAName = "",
     [Parameter(Mandatory=$true)]
-    [string]$BUILD_NUMBER = "",
-    [string]$CLEAN_MODE = "uninstall",
+    [string]$BuidlVersion = "",
+    [string]$CleanMode = "uninstall",
     [string]$SUBSCRIPTION_ID = ""
 )
 
 $txtuser=$env:CSAAccount
 $txtpwd=$env:CSAPassword
 
-switch($CLEAN_MODE) 
+switch($CleanMode) 
 {
     "resnapshot" {break}
     "uninstall" {}
     default {
-        $csaDeployment = [CSAMachineDeployUninstall]::new($VM_NAME,$SUBSCRIPTION_ID,$txtuser,$txtpwd)
-        $csaDeployment.DeployWithBuildVersion($BUILD_NUMBER)
+        $csaDeployment = [CSAMachineDeployUninstall]::new($CSAName,$SUBSCRIPTION_ID,$txtuser,$txtpwd)
+        $csaDeployment.DeployWithBuildVersion($BuidlVersion)
         break
     }
 }
