86b9fa78335acfad1f63ff76903629d424e7d024
Guo, Yi-Bin (Kim, HPSW-R&D-SH) <yguo@hpe.com>
2018-08-07 13:30:18 +0800

    update script

--- mydastr01cleaner.sh
+++ mydastr01cleaner.sh
@@ -8,44 +8,48 @@ remove_expired_folders()
     productname=$2
     configname=$3
     basedir=/products/${groupname}/${productname}/${configname}
+    outputdir=/products/${groupname}
     if [ ! -d "${basedir}" ]; then
-        echo "not existing ${basedir}"
+        echo "not existing config folder ${configname}"
         return
     fi
-    find ${basedir}/ -maxdepth 1 -type l -printf "%f\n" > ${productname}_${configname}_lk.txt
-    find ${basedir}/ -maxdepth 1 -mtime +7 -type d -printf "%f\n" > ${productname}_${configname}_dir.txt
+    find ${basedir}/ -maxdepth 1 -type l -printf "%f\n" > ${outputdir}/${productname}_${configname}_lk.txt
+    find ${basedir}/ -maxdepth 1 -mtime +7 -type d -printf "%f\n" > ${outputdir}/${productname}_${configname}_dir.txt
 
 
-    if [ -f ${productname}_${configname}_keep_dir.txt ]; then
-        rm -f ${productname}_${configname}_keep_dir.txt
+    if [ -f ${outputdir}/${productname}_${configname}_keep_dir.txt ]; then
+        rm -f ${outputdir}/${productname}_${configname}_keep_dir.txt
     fi
     while IFS='' read -r line || [[ -n "$line" ]]; do
-        echo $line
         if [ "" != "$line" ]; then
-            readlink ${basedir}/$line >> ${productname}_${configname}_keep_dir.txt
+            readlink ${basedir}/$line >> ${outputdir}/${productname}_${configname}_keep_dir.txt
         fi
-    done < "${productname}_${configname}_lk.txt"
+    done < "${outputdir}/${productname}_${configname}_lk.txt"
 
+    echo "keep the folders below:"
+    cat  ${outputdir}/${productname}_${configname}_keep_dir.txt
 
     while IFS='' read -r line || [[ -n "$line" ]]; do
         findres=$(cat ${productname}_${configname}_keep_dir.txt | grep ${line})
         if [ "${findres}" == "" ]; then
-            echo ${line} >> ${productname}_${configname}_remove_dir.txt
+            echo ${line} >> ${outputdir}/${productname}_${configname}_remove_dir.txt
         fi 
-    done < "${productname}_${configname}_dir.txt"
+    done < "${outputdir}/${productname}_${configname}_dir.txt"
 
     echo "folders will be removed! in ${groupname} ${productname} ${configname}"
-    cat ${productname}_${configname}_remove_dir.txt
+    cat ${outputdir}/${productname}_${configname}_remove_dir.txt
 }
 
 find /products/${groupfolder}/* -type d -maxdepth 1 -printf "%f\n" | 
     while IFS='' read -r productfolder || [[ -n "$productfolder" ]]; do
-        echo "productfolder = ${productfolder}"
+        echo "product folder = ${productfolder}"
         if [ -d "/products/${groupfolder}/${productfolder}" ]; then
             for cfg in ${arycfg[@]}; do
                 echo "config folder = ${cfg}"
                 remove_expired_folders $groupfolder $productfolder $cfg
             done
+        else        
+            echo "not existing the product folder ${productfolder}"
         fi   
     done
 # for productfolder in $(find /products/${groupfolder}/* -type d -maxdepth 1 -printf "%f\n"); do
