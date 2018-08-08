aa2219992947eb56bea990ece9c334899ef86db8
Guo, Yi-Bin (Kim, HPSW-R&D-SH) <yguo@hpe.com>
2018-08-08 09:52:10 +0800

    renew the ignore list

--- mydastr01cleaner.sh
+++ mydastr01cleaner.sh
@@ -7,7 +7,7 @@ ignoreprods="@LT-PCQC@LT-PCQC-FIST@LeanFT@"
 search_ignore_list() 
 {
     productname=$1
-    ignoreprods=("LT-PCQC" "LT-PCQC-FIST" "LeanFT" "STM")
+    ignoreprods=("LT-PCQC" "LT-PCQC-FIST" "LeanFT")
 
     for ignoreprod in "${ignoreprods[@]}"; do
         if [[ "$productname" == "$ignoreprod" ]]; then
