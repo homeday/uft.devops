d712ebea21d91523ffc1779f269bb06a8f7a8203
Xu, Yuanhai (Solever, HPSW-R&D-SH) <yuanhai.xu@hpe.com>
2018-11-08 11:19:14 +0800

    Add uft code-freeze-only email template; email: hpe -> microfocus

--- jenkinsmailtemplate/mf-cd-html.uft.code-freeze-only.groovy
+++ jenkinsmailtemplate/mf-cd-html.uft.code-freeze-only.groovy
@@ -42,12 +42,6 @@ def unlock_job_url = "http://mydtbld0120.hpeswlab.net:8080/view/Products/view/Gi
 def status_checks = "cd/releases/${rel_major_num}_${rel_minor_num}/code_freeze"
 %>
 
-
-    <h2>UFT ${release_num} Sprint ${sprint_num} sanity has passed!</h2>
-
-<%
-if (code_freeze_phase) {
-%>
     <p>
         <span class="emphasize2">SPECIAL REMINDER:</span> <span class="emphasize3">WE ARE IN UFT ${release_num} CODE FREEZE PHASE!</span><br/><br/>
         Approvals from <a href="mailto:vika.milgrom@microfocus.com"><span class="emphasize2">Vika</span></a>,
@@ -57,16 +51,6 @@ if (code_freeze_phase) {
         <a href="mailto:jia.xue2@microfocus.com"><span class="emphasize2">James</span></a> are required for any exceptions
         before using <a href="${unlock_job_url}">force unlock job</a> (context: <code>${status_checks}</code>)!
     </p>
-<%
-} else {
-%>
-    <p>
-        Code freeze is <span class="emphasize">&nbsp;OFF&nbsp;</span> now!<br/>
-        You are able to merge your approved pull requests to official branches.
-    </p>
-<%
-}
-%>
 
     <p>
         <br/>
