7a050e342910bf123d102f06196de4324e924085
Guo YiBin <yguo@microfocus.com>
2019-05-14 18:05:01 +0800

    net use problem

--- deployment/deploymentscripts/installUFT_Patch.bat
+++ deployment/deploymentscripts/installUFT_Patch.bat
@@ -1,6 +1,8 @@
 net use * /delete /y
-IF NOT EXIST P: ECHO P: was not mounted. mounting it to \\\mydastr01.hpeswlab.net\products & net use P: \\\mydastr01.hpeswlab.net\products %4 /USER:%3
-set PATCHDIR=P:\FT\QTP\win32_release\%1\Patches\UFT\%2\PatchResult\default
+REM IF NOT EXIST P: ECHO P: was not mounted. mounting it to \\\mydastr01.hpeswlab.net\products & net use P: \\\mydastr01.hpeswlab.net\products %4 /USER:%3
+SET STORAGE_WIN_SERVER=\\mydastr01.hpeswlab.net
+net use %STORAGE_WIN_SERVER%\IPC$ %4 /USER:%3
+set PATCHDIR=%STORAGE_WIN_SERVER%\products\FT\QTP\win32_release\%1\Patches\UFT\%2\PatchResult\default
 pushd %PATCHDIR%
 %2.exe /s /e /f C:\temp\%2
 popd 
