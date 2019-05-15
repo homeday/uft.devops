405b7ff3bef1ee96cc973c56cb4deb2a4c0949e1
Guo YiBin <yguo@microfocus.com>
2019-05-15 17:52:14 +0800

    fixed

--- deployment/deploymentscripts/CSAInstallApp.psm1
+++ deployment/deploymentscripts/CSAInstallApp.psm1
@@ -122,7 +122,7 @@ class CSAInstallUFT : CSAInstallApp {
                 $result = Invoke-Command -ComputerName $CSAName -Credential $CSACredential {Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Mercury Interactive\QuickTest Professional\CurrentVersion"}
                 Write-Host "Check Version result =  ${result}" -ForegroundColor Green -BackgroundColor Black
                 if ($result -ne $null) {
-                    $versionInreg = "${result.Major}.${result.Minor}.${result.build}.0"
+                    $versionInreg = $result.Major + "." + $result.Minor + "." + $result.build + ".0"
                     Write-Host "versionInreg =  ${versionInreg}" -ForegroundColor Green -BackgroundColor Black
                     $res = $versionInreg -eq $BuildVersion
                     Write-Host "res = $res" -ForegroundColor Green -BackgroundColor Black
