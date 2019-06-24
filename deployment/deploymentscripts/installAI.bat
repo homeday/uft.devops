0628d8b99f782a49164e216ae55cab5a85f44804
Guo YiBin <yguo@microfocus.com>
2019-06-24 21:03:00 +0800

    update ai install script

--- deployment/deploymentscripts/installAI.bat
+++ deployment/deploymentscripts/installAI.bat
@@ -11,26 +11,16 @@ IF "%ProgramFiles(x86)%"=="" set MSI_FILE_NAME=Unified_Functional_Testing_x86.ms
 IF NOT "%ProgramFiles(x86)%"=="" set MSI_FILE_NAME=Unified_Functional_Testing_x64.msi
 
 
-set FULL_PATH=%CURR_PATH%
-set FULL_PATH=%DVD_Path%\
-set LicenseAddress=%2
 
-IF NOT EXIST "%FULL_PATH%%MSI_FILE_NAME%" (
-	echo "Can't find the installer package %FULL_PATH%%MSI_FILE_NAME%"
-	goto ERROR
-)
+
+set LicenseAddress=%2
 
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set currdate=%%c_%%a_%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set currtime=%%a%%b)
 SET MSI_LOG_FILE_NAME=UFTSetup_%currdate%_%currtime%.log
 SET MSI_LOG_FILE_NAME=%MSI_LOG_FILE_NAME: =_%
 
-echo Checking KB
-cmd /c "%FULL_PATH%prerequisites\vc2015_redist_x86\VC2015Prerequisite.exe" silent
-IF ERRORLEVEL 1 (
-	echo Missing some KB
-	goto ERROR
-)
+
 
 pushd %STORAGE_WIN_SERVER%\products\FT\QTP\win32_release\%1\SetupBuilder\Output\UFT\prerequisites
 cmd /c setup.exe /InstallOnlyPrerequisite /s
@@ -39,8 +29,9 @@ popd
 echo Installing UFT
 set AddinsToInstall=Core_Components,Web_Add_in,ALM_Plugin,IDE,Test_Results_Viewer,Samples,ActiveX_Add_in,Visual_Basic_Add_in,Delphi_Add_in,Flex_Add_in,Java_Add_in,_Net_Add_in,Oracle_Add_in,PeopleSoft_Add_in,PowerBuilder_Add_in,Qt_Add_in,SAP_Solutions_Add_in,SAP_eCATT_integration,Siebel_Add_in,Stingray_Add_in,TE_Add_in,VisualAge_Add_in,RPA
 set UFTConfiguration=CONF_MSIE=1 ALLOW_RUN_FROM_ALM=1 ALLOW_RUN_FROM_SCRIPTS=1 DLWN_SCRIPT_DBGR=1
-cmd /c MsiExec /norestart /qn /i "Z:\FT\QTP\win32_release\%1\DVD_WIX\Unified Functional Testing\MSI\Unified_Functional_Testing_x64.msi" /l*xv C:\UFT_Install_Log.txt %UFTConfiguration% LICSVR=%LicenseAddress% LICID=23078 ADDLOCAL=%AddinsToInstall%
-
+pushd %STORAGE_WIN_SERVER%\products\FT\QTP\win32_release
+cmd /c MsiExec /norestart /qn /i "Z:\FT\QTP\win32_release\%1\DVD_WIX\Unified Functional Testing\MSI\%MSI_FILE_NAME%" /l*xv C:\UFT_Install_Log.txt %UFTConfiguration% LICSVR=%LicenseAddress% LICID=23078 ADDLOCAL=%AddinsToInstall%
+popd
 IF ERRORLEVEL 1 (
 	echo Install UFT error
 	goto ERROR
@@ -50,10 +41,13 @@ echo Installing AI
 pushd %STORAGE_WIN_SERVER%\products\FT\QTP\win32_release\%1\DVD_Wix\AI
 cmd /c setup.exe /InstallOnlyPrerequisite /s
 popd 
-
+pushd %STORAGE_WIN_SERVER%\products\FT\QTP\win32_release
 cmd /c MsiExec /norestart /qn /i "Z:\FT\QTP\win32_release\%1\DVD_WIX\AI\MSI\AI_Installer.msi" /l*xv C:\AI_Install_Log.txt ADDLOCAL=AI_Services,UI_Services
-
-
+popd
+IF ERRORLEVEL 1 (
+	echo Install UFT error
+	goto ERROR
+)
 goto END
 
 :END
