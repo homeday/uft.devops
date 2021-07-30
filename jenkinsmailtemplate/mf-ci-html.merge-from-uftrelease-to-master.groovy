a542dfd501032ec6cf681ff4274268db7c2b96f1
Yuanhai(Solever) Xu <yuanhai.xu@microfocus.com>
2021-07-30 20:00:47 +0800

    update email template

--- jenkinsmailtemplate/mf-ci-html.merge-from-uftrelease-to-master.groovy
+++ jenkinsmailtemplate/mf-ci-html.merge-from-uftrelease-to-master.groovy
@@ -21,6 +21,8 @@
 		.UNSTABLE {color: Yellow;}
 		.shadow {border-radius: 5px; box-shadow: 1px 1px 1px 1px darkgray; }
 		.build_detail_td { width:25%;}
+		.merge-result.not-merged {color: Red;}
+		.repo-Exclude {color: Red;}
 	  </style>
 	
 	<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="margin: 0px; padding: 0px; background-color: #e2e2e2;" bgcolor="#e2e2e2">
@@ -45,6 +47,9 @@
 			def excludeRepos = env.exclude_repos
 
 			def mergeResultsFile = build.getWorkspace().child("merge_results.txt")
+
+			def dismissUrlBase = "http://mydtbld0211.swinfra.net:8080/job/Merge.From.UFTRelease.To.master.CommitDismission/buildWithParameters?token=UFT_TOKEN"
+			dismissUrlBase.concat("&cause=").concat(java.net.URLEncoder.encode("Via link from merge job email", "UTF-8"))
 		%>
 		<table width="100%" cellpadding="0" cellspacing="0"><tr><td valign="center"><tr><td>
 			<table style="background-color: #FFF" bgcolor="#FFF" class="shadow" width="80%" align="center" cellpadding="5" cellspacing="0">
@@ -92,10 +97,16 @@
 						<br />
 					</td>
 				</tr>
+				<br />
 
 				<!-- merged repositories -->
 				<tr>
 					<td>
+						<span>Please merge the <b>Exclude</b> repositories manually if any changes are detected!</span>
+						<br />
+						<span>You can also dismiss the <b>Not Merged</b> commit if it is certain that the commit shall never be merged. 
+						A blank page will be shown when the dismission link is clicked.</span>
+						<br />
 						<table class="shadow" width="100%" style="border: 1px solid #e2e2e2" cellpadding="0" cellspacing="0">
 							<tr style="background-color: #A8A8A8" bgcolor="#A8A8A8" ><td><h3 style="color: White">Merged Repositories</h3></td></tr>
 							<tr><td><table width="100%" cellpadding="0" cellspacing="0">
@@ -105,9 +116,10 @@
 
 								<tr style="border-bottom: 1px solid #e2e2e2" >
 									<th align="left">Repository</th>
-									<th align="left">Number of Commits Ahead</th>
+									<th align="left">Commits Ahead</th>
 									<th align="left">Merge Result</th>
 									<th align="left">Include/Exclude</th>
+									<th align="left">Dismission</th>
 								</tr>
 
 								<% 		
@@ -115,17 +127,32 @@
 											def splitArray = line.split(',')
 											def repo = splitArray[0]
 											def incOrExc = splitArray[1]
-											def aheadBefore = splitArray[2]
-											def aheadAfter = splitArray[3]
+											def commitSHA = splitArray[2]
+											def dimissionState = splitArray[3]
+											def aheadBefore = splitArray[4]
+											def aheadAfter = splitArray[5]
 											if (aheadBefore == '' || aheadBefore == '0') return
-											def mergeResult = (aheadAfter == '' || aheadAfter == '0') ? 'Merged' : 'Not Merged'
+											def isMerged = aheadAfter == '' || aheadAfter == '0'
+											def mergeResultClass = isMerged ? 'merged' : 'not-merged'
+											def mergeResultText = isMerged ? 'Merged' : 'Not Merged'
+											def dismissFullUrl = dismissUrlBase
+												.concat("&Repository=").concat(java.net.URLEncoder.encode(repo, "UTF-8"))
+												.concat("&BranchName=").concat(java.net.URLEncoder.encode(sourceBranch, "UTF-8"))
+												.concat("&CommitSHA=").concat(java.net.URLEncoder.encode(commitSHA, "UTF-8"))
 								%>
 
 								<tr>
 									<td valign="top">${repo}</td>
 									<td valign="top">${aheadBefore}</td>
-									<td valign="top">${mergeResult}</td>
-									<td valign="top">${incOrExc}</td>
+									<td valign="top"><span class="merge-result ${mergeResultClass}">${mergeResultText}</span></td>
+									<td valign="top"><span class="repo-${incOrExc}">${incOrExc}</span></td>
+									<td valign="top">
+								<%			if (dimissionState == "Dismissed") { %>
+										<span><b>Dismissed (No need to merge)</b></span>
+								<%			} else { %>
+										<a href="${dismissFullUrl}" target="_blank">Don't merge this commit ${commitSHA}</a>
+								<%			} %>
+									</td>
 								</tr>
 
 								<% 		
