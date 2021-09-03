96b18cf4496084a159f3054037e0831649d02b3b
Yuanhai(Solever) Xu <yuanhai.xu@microfocus.com>
2021-09-03 16:51:33 +0800

    Update email template

--- jenkinsmailtemplate/mf-ci-html.merge-from-uftrelease-to-master.groovy
+++ jenkinsmailtemplate/mf-ci-html.merge-from-uftrelease-to-master.groovy
@@ -25,6 +25,7 @@
 		.merge-result.not-merged {color: Red;}
 		.repo-Include {color: Green;}
 		.repo-Exclude {color: Red;}
+		.error-message {color: Red; font-weight: bold;}
 	  </style>
 	
 	<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="margin: 0px; padding: 0px; background-color: #e2e2e2;" bgcolor="#e2e2e2">
@@ -49,6 +50,7 @@
 			def excludeRepos = env.exclude_repos
 
 			def mergeResultsFile = build.getWorkspace().child("merge_results.txt")
+			def errorFile = build.getWorkspace().child("error.txt")
 
 			def dismissUrlBase = "http://mydtbld0211.swinfra.net:8080/job/Merge.From.UFTRelease.To.master.CommitDismission/buildWithParameters?token=UFT_TOKEN"
 			dismissUrlBase.concat("&cause=").concat(java.net.URLEncoder.encode("Via link from merge job email", "UTF-8"))
@@ -116,24 +118,44 @@
 
 								<tr style="border-bottom: 1px solid #e2e2e2" >
 									<th align="left">Repository</th>
+									<th align="left">Include/Exclude</th>
 									<th align="left">Ahead</th>
 									<th align="left">Last Commit</th>
 									<th align="left">Merge Result</th>
-									<th align="left">Include/Exclude</th>
 									<th align="left">Dismission</th>
 								</tr>
 
 								<% 		
 										mergeResultsFile.readToString().eachLine(){ line -> 
 											def splitArray = line.split(',')
-											def repo = splitArray[0]
-											def incOrExc = splitArray[1]
-											def commitSHA = splitArray[2]
-											def dimissionState = splitArray[3]
-											def aheadBefore = splitArray[4]
-											def aheadAfter = splitArray[5]
-											if (aheadBefore == '' || aheadBefore == '0') return
-											def isMerged = aheadAfter == '' || aheadAfter == '0'
+											def repo = splitArray.size() > 0 ? splitArray[0] : ''
+											def incOrExc = splitArray.size() > 1 ? splitArray[1] : ''
+
+											def commitSHAOrErrorInd = splitArray.size() > 2 ? splitArray[2] : ''
+											if (commitSHAOrErrorInd == '[ERROR]') {
+												def errorMessage = splitArray.size() > 3 ? splitArray[3..-1].join(',') : ''
+								%>
+
+								<tr>
+									<td valign="top">${repo}</td>
+									<td valign="top"><span class="repo-${incOrExc}">${incOrExc}</span></td>
+									<td class="error-message" valign="top" colspan="4">${errorMessage}</td>
+								</tr>
+
+								<%
+												return
+											}
+
+											def commitSHA = commitSHAOrErrorInd
+											if (commitSHA == 'null') { commitSHA = '' }
+											def dimissionState = splitArray.size() > 3 ? splitArray[3] : ''
+											if (dimissionState == 'null') { dimissionState = '' }
+											def aheadBefore = splitArray.size() > 4 ? splitArray[4] : '0'
+											if (aheadBefore == '' || aheadBefore == 'null') { aheadBefore = '0' }
+											def aheadAfter = splitArray.size() > 5 ? splitArray[5] : '0'
+											if (aheadAfter == '' || aheadAfter == 'null') { aheadAfter = '0' }
+											def isMerged = aheadBefore != '0' && aheadAfter == '0'
+											if (incOrExc == 'Include' && !isMerged && aheadBefore == '0') return
 											def mergeResultClass = isMerged ? 'merged' : 'not-merged'
 											def mergeResultText = isMerged ? 'Merged' : 'Not Merged'
 											def dismissFullUrl = dismissUrlBase
@@ -141,22 +163,20 @@
 												.concat("&BranchName=").concat(java.net.URLEncoder.encode(sourceBranch, "UTF-8"))
 												.concat("&CommitSHA=").concat(java.net.URLEncoder.encode(commitSHA, "UTF-8"))
 
-											def commitShortSHA = commitSHA[0..6]
+											def commitShortSHA = commitSHA.length() >= 7 ? commitSHA[0..6] : commitSHA
 											def commitGHEUrl = "https://github.houston.softwaregrp.net/uft/${repo}/commit/${commitSHA}"
 								%>
 
 								<tr>
 									<td valign="top">${repo}</td>
+									<td valign="top"><span class="repo-${incOrExc}">${incOrExc}</span></td>
 									<td valign="top">${aheadBefore}</td>
 									<td valign="top"><a href="${commitGHEUrl}" target="_blank">${commitShortSHA}</a></td>
 									<td valign="top"><span class="merge-result ${mergeResultClass}">${mergeResultText}</span></td>
-									<td valign="top"><span class="repo-${incOrExc}">${incOrExc}</span></td>
 									<td valign="top">
-								<%			if (isMerged) { %>
-										<span></span>
-								<%			} else if (dimissionState == "Dismissed") { %>
+								<%			if (dimissionState == "Dismissed") { %>
 										<span><b>Dismissed (No need to merge)</b></span>
-								<%			} else { %>
+								<%			} else if (!isMerged && incOrExc == 'Include' && aheadBefore != '0') { %>
 										<a href="${dismissFullUrl}" target="_blank">Don't merge this commit ${commitSHA}</a>
 								<%			} %>
 									</td>
