75a9a7ded6511c135475fddb6eb34a000ec6d683
NarendraKumar.Chejara.60036163 <NarendraKumar.Chejara@microfocus.com>
2022-10-18 11:35:01 +0800

    update AI installation (#84)

--- deployment/awsDeploymentScript/install_codeless.bat
+++ deployment/awsDeploymentScript/install_codeless.bat
@@ -7,21 +7,27 @@ fsutil behavior set SymlinkEvaluation R2R:1
 
 SET STORAGE_WIN_SERVER=\\rubicon.cross.admlabs.aws.swinfra.net\fsx
 net use P: %STORAGE_WIN_SERVER%\products /PERSISTENT:YES
-
-echo *********************************
 net use
+
+robocopy /s /e P:\FT\CDLS-AI\win32_release\%1\DVD_Remote C:\Installation_uft\CDLS-AI\%1\DVD /MT:16 /R:5 /NDL /NFL
+set DVD_Path=C:\Installation_uft\CDLS-AI\%1\DVD
+
+net use * /delete /y
+
+echo *********************************
+echo Installing prerequisites
 echo *********************************
 
-ECHO Installing AI
-pushd P:\FT\CDLS-AI\win32_release\%1\DVD
+pushd %DVD_Path%
 cmd /c setup.exe /InstallOnlyPrerequisite /s
-popd 
 
-pushd P:\FT\QTP\win32_release
-cmd /c MsiExec /norestart /qn /i "%STORAGE_WIN_SERVER%\products\FT\CDLS-AI\win32_release\%1\DVD\AI_Installer.msi" /l*xv C:\AI_Install_Log.txt 
+echo *********************************
+echo Installing AI
+echo *********************************
+
+cmd /c MsiExec /norestart /qn /i "%DVD_Path%\AI_Installer.msi" /l*xv C:\AI_Install_Log.txt 
 SET ERRORCODE=%ERRORLEVEL%
+
 popd
-
-
 exit 0
 
