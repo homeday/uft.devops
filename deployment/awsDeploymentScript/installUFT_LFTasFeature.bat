ee57714383127b732e8f2ecd471787024a11b37a
NarendraKumar.Chejara.60036163 <NarendraKumar.Chejara@microfocus.com>
2024-05-14 10:58:45 +0800

    remove vs2015 plugin (#133)

--- deployment/awsDeploymentScript/installUFT_LFTasFeature.bat
+++ deployment/awsDeploymentScript/installUFT_LFTasFeature.bat
@@ -41,7 +41,7 @@ echo %AddinsToInstall%
 
 
 set UFTConfiguration=CONF_MSIE=1 ALLOW_RUN_FROM_ALM=1 ALLOW_RUN_FROM_SCRIPTS=1 DLWN_SCRIPT_DBGR=1
-set LeanFTConfiguration=UFTDeveloper,UFTDeveloper_Engine,UFTDeveloper_Client,Vs2015Addin,IntelliJAddin,EclipseAddin ECLIPSE_INSTALLDIR="C:\DevTools\eclipse"
+set LeanFTConfiguration=UFTDeveloper,UFTDeveloper_Engine,UFTDeveloper_Client,IntelliJAddin,EclipseAddin ECLIPSE_INSTALLDIR="C:\DevTools\eclipse"
 set LicenseAddress=%2
 
 
